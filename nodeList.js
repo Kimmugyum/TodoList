84abed4c928d3a52d50e7c92301269522ecacde1
Kimmugyum <gyum2726@naver.com>
2020-01-22 19:44:24 +0900

    javscript 수정

--- nodeList.js
+++ nodeList.js
@@ -6,14 +6,14 @@ function add()
     var line = document.getElementById("line")
     var boss = document.getElementById("boss") // 전체 부모
     boss.innerHTML += `
-    <div class = "clean">
+    <div>
         <input type = "checkbox" onclick = "addEventListener('click', checked)">
         <span>${line.value}</span>
-        <input type ="button" class = "delete" onclick = "addEventListener('click', clickHandler)" value = "삭제">
+        <input type ="button" class = "delete" onclick = "addEventListener('click', garbage)" value = "삭제">
     </div>`
     line.value = "" // input에 값은 value를 쓰고 input값이 아닌것은 innerText or innerHTML쓴다.
 }
-function clickHandler(event)
+function garbage(event)
 {   
     var boss = document.getElementById("boss")
     boss.removeChild(event.target.parentNode)
@@ -21,29 +21,23 @@ function clickHandler(event)
 
 function checked(event)
 {   
-     if(event.target.checked == true){
+    if(event.target.checked == true)
+    {
         this.nextElementSibling.style.textDecoration = "line-through";
-        }
-        else
-        {
-        this.nextElementSibling.style.textDecoration = "none"
-        }
+    }
+    else
+    {
+       this.nextElementSibling.style.textDecoration = "none"
+    }
 }
-//         if(event.target.checked == true){
-//         event.target.style.textDecoration = "line-through";
-//         }
-//         else
-//         {
-//             event.target.nextsibling.style.textDecoration = "none"
-//         }
-// }
+
 var discard = document.getElementsByClassName("delete")
-    for(var i = 0; i < discard.length; i++){
-        discard[i].addEventListener('click', clickHandler)
+for(var i = 0; i < discard.length; i++){
+    discard[i].addEventListener('click', garbage)
 }
 
 var box = document.getElementsByClassName("box")
-    for(var i = 0; i < box.length; i++)
-    {
-        box[i].addEventListener('click', checked)
-    }
+for(var i = 0; i < box.length; i++)
+{
+    box[i].addEventListener('click', checked)
+}
