f674b1b7700bee0fb184af7439d91742cc032735
Kimmugyum <gyum2726@naver.com>
2020-01-23 02:25:51 +0900

    javascript 수정

--- nodeList.js
+++ nodeList.js
@@ -1,21 +1,31 @@
 var blue = document.getElementById("blue")
 blue.addEventListener('click', add)
-linevalue("할일")
+// var todoList = [{
+//     ''
+// }]
+//배열로 하기
+lineValue("할일")
+lineValue("할일2")
+lineValue(3)
+
 function add()
 {
     var line = document.getElementById("line")
-    linevalue(line.value)
+    lineValue(line.value)
     line.value = "" // input에 값은 value를 쓰고 input값이 아닌것은 innerText or innerHTML쓴다.
-}
-function linevalue(a)
+} 
+function lineValue(value)
 {
     var boss = document.getElementById("boss") // 전체 부모
+    // for(var i =0; i<)
     boss.innerHTML += `
     <div>
         <input type = "checkbox" onclick = "addEventListener('click', checked)">
-        <span>${a}</span>
-        <input type ="button" class = "delete" onclick = "addEventListener('click', garbage)" value = "삭제">
+        <span>${value}</span>
+        <i class="fas fa-trash-alt delete"></i>
     </div>`
+    var discard = document.querySelector('div:last-child .delete')
+    discard.addEventListener('click', garbage)
 }
 function garbage(event)
 {   
@@ -35,14 +45,10 @@ function checked(event)
     }
 }
 
-var discard = document.getElementsByClassName("delete")
-for(var i = 0; i < discard.length; i++){
-    discard[i].addEventListener('click', garbage)
-}
+
 
 var box = document.getElementsByClassName("box")
 for(var i = 0; i < box.length; i++)
 {
     box[i].addEventListener('click', checked)
 }
-add()
