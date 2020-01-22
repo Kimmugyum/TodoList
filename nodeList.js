c959fc36ffbadac2ab6ef831116648402d2a87c1
Kim Mugyum <gyum2726@naver.com>
2020-01-22 17:49:53 +0900

    javascript 수정

--- nodeList.js
+++ nodeList.js
@@ -7,33 +7,36 @@ function add()
     var boss = document.getElementById("boss") // 전체 부모
     boss.innerHTML += `
     <div class = "clean">
-        <input type = "checkbox">
+        <input type = "checkbox" onclick = "addEventListener('click', checked)">
         <span>${line.value}</span>
-        <input type ="button" class = "delete" value = "삭제">
+        <input type ="button" class = "delete" onclick = "addEventListener('click', clickHandler)" value = "삭제">
     </div>`
     line.value = "" // input에 값은 value를 쓰고 input값이 아닌것은 innerText or innerHTML쓴다.
 }
 function clickHandler(event)
-{
+{   
     var boss = document.getElementById("boss")
     boss.removeChild(event.target.parentNode)
 }
 
-function checked()
+function checked(event)
 {   
-    var span = document.getElementsByTagName("span")
-    
-    for(var i =0; i < span.length; i++)
-    {
-        if(box[i].checked == true){
-        span[i].style.textDecoration = "line-through";
+     if(event.target.checked == true){
+        this.nextElementSibling.style.textDecoration = "line-through";
         }
         else
         {
-            span[i].style.textDecoration = "none"
+        this.nextElementSibling.style.textDecoration = "none"
         }
-    }
 }
+//         if(event.target.checked == true){
+//         event.target.style.textDecoration = "line-through";
+//         }
+//         else
+//         {
+//             event.target.nextsibling.style.textDecoration = "none"
+//         }
+// }
 var discard = document.getElementsByClassName("delete")
     for(var i = 0; i < discard.length; i++){
         discard[i].addEventListener('click', clickHandler)
