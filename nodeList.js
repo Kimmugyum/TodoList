7b201486d1f0e1d96f9599089a7c54fc86c0b78f
Kimmugyum <gyum2726@naver.com>
2020-01-22 20:03:49 +0900

    javasciprt html 수정

--- nodeList.js
+++ nodeList.js
@@ -1,17 +1,21 @@
 var blue = document.getElementById("blue")
 blue.addEventListener('click', add)
-
+linevalue("할일")
 function add()
 {
     var line = document.getElementById("line")
+    linevalue(line.value)
+    line.value = "" // input에 값은 value를 쓰고 input값이 아닌것은 innerText or innerHTML쓴다.
+}
+function linevalue(a)
+{
     var boss = document.getElementById("boss") // 전체 부모
     boss.innerHTML += `
     <div>
         <input type = "checkbox" onclick = "addEventListener('click', checked)">
-        <span>${line.value}</span>
+        <span>${a}</span>
         <input type ="button" class = "delete" onclick = "addEventListener('click', garbage)" value = "삭제">
     </div>`
-    line.value = "" // input에 값은 value를 쓰고 input값이 아닌것은 innerText or innerHTML쓴다.
 }
 function garbage(event)
 {   
@@ -41,3 +45,4 @@ for(var i = 0; i < box.length; i++)
 {
     box[i].addEventListener('click', checked)
 }
+add()
