d3afeeeacd7d664824f1d4eb15b9deeef41688c2
Kim Mugyum <gyum2726@naver.com>
2020-01-28 14:01:17 +0900

    배열 사용 및 appendChild로 수정

--- nodeList.js
+++ nodeList.js
@@ -1,12 +1,11 @@
 var blue = document.getElementById("blue")
 blue.addEventListener('click', add)
-// var todoList = [{
-//     ''
-// }]
-//배열로 하기
-lineValue("할일")
-lineValue("할일2")
-lineValue(3)
+
+ var todoList = ['할일','할일2','3']
+
+lineValue(todoList[0])
+lineValue(todoList[1])
+lineValue(todoList[2])
 
 function add()
 {
@@ -17,16 +16,24 @@ function add()
 function lineValue(value)
 {
     var boss = document.getElementById("boss") // 전체 부모
-    // for(var i =0; i<)
-    boss.innerHTML += `
-    <div>
-        <input type = "checkbox" onclick = "addEventListener('click', checked)">
-        <span>${value}</span>
-        <i class="fas fa-trash-alt delete"></i>
-    </div>`
+        var a = document.createElement("div")
+        var b = document.createElement("input")
+        a.appendChild(b)
+        b.setAttribute("type","checkbox")
+        b.setAttribute("class","box")
+        var c = document.createElement("span")
+        var d = document.createTextNode(value)
+        c.appendChild(d)
+        a.appendChild(c)
+        var e = document.createElement("i")
+        e.setAttribute("class","fas fa-trash-alt delete")
+        a.appendChild(e)     
+        boss.appendChild(a)
+
     var discard = document.querySelector('div:last-child .delete')
     discard.addEventListener('click', garbage)
 }
+
 function garbage(event)
 {   
     var boss = document.getElementById("boss")
@@ -44,8 +51,6 @@ function checked(event)
        this.nextElementSibling.style.textDecoration = "none"
     }
 }
-
-
 
 var box = document.getElementsByClassName("box")
 for(var i = 0; i < box.length; i++)
